e39ef6fc2c23cce291d537aed188a3091da256cf
pheeria <olzhas.askar@deliveryhero.com>
2021-12-29 00:51:52 +0100

    chore: add dependencies

--- project.clj
+++ project.clj
@@ -1,9 +1,12 @@
 (defproject tweets "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Trying to use Clojure for Tweets"
+  :url "https://twitter.com/"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [
+                 [org.clojure/clojure "1.10.1"]
+                 [org.clojure/data.json "2.4.0"]
+                 [http-kit "2.6.0-alpha1"]]
   :main ^:skip-aot tweets.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
