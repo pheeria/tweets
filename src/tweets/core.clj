5b4f5936938c72d37c083456d1015c6d27159388
pheeria <olzhas.askar@deliveryhero.com>
2021-12-31 15:08:08 +0100

    feat: server and a handmade router

--- src/tweets/core.clj
+++ src/tweets/core.clj
@@ -1,10 +1,43 @@
 (ns tweets.core
   (:require org.httpkit.client
+            [org.httpkit.server :refer [run-server]]
             [tweets.twitter :as twitter]
+            [tweets.config :as config]
+            [clojure.data.json :as json]
             [org.httpkit.sni-client :as sni-client]))
 
 ;; Change default client for your whole application:
 (alter-var-root #'org.httpkit.client/*default-client* (fn [_] sni-client/default-client))
 
-(defn -main []
-  (twitter/fetch-tweets))
+(defonce server (atom nil))
+
+(defn not-found [_]
+  {:status 404
+   :body "Not Found"})
+
+(defn meaning [_]
+  {:status 200
+   :headers {"Content-Type" "application/json"}
+   :body "{\"answer\": 42}"})
+
+(defn tweets [_]
+  (let [response (twitter/fetch-tweets)]
+    {:status 200
+     :headers {"Content-Type" "application/json"}
+     :body (json/write-str response)}))
+
+(defn app [req]
+  (let [uri (:uri req)
+        handler (case uri
+                  "/meaning" meaning
+                  "/tweets" tweets
+                  not-found)]
+    (handler req)))
+
+(defn stop-server []
+  (when (some? @server)
+    (@server :timeout 100)
+    (reset! server nil)))
+
+(defn -main [& args]
+  (reset! server (run-server #'app {:port config/port})))
